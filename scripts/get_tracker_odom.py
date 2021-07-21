d611e3effada662e2ca5a9af9438bf454f6872b2
jypark <jiyoonpark0207@gmail.com>
2021-07-21 18:23:59 +0900

    add print files
    formatted msgs

---
+++ scripts/get_tracker_odom.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import rospy
+from nav_msgs.msg import Odometry
+
+def get_current_position(msg):
+    # follows the conventional x, y, poses
+    twist_linear = msg.twist.twist.linear
+    linear_x = twist_linear.x
+    linear_y = twist_linear.y
+    linear_z = twist_linear.z
+
+    twist_angular = msg.twist.twist.angular
+    angular_x = twist_angular.x
+    angular_y = twist_angular.y
+    angular_z = twist_angular.z
+
+    import time
+    seconds = int(round(time.time()*10))
+    # time.sleep(0.5)
+    # print(seconds)
+    if seconds % 30 == 0:
+
+        print("x: {:.3f} y: {:.3f} w: {:.3f}".format(linear_x, linear_y, linear_z ))
+        print("x: {:.3f} y: {:.3f} w: {:.3f}".format(angular_x, angular_y, angular_z ))
+        print("=======================================================================")
+
+    else:
+        pass
+        
+
+
+if __name__=="__main__":
+    
+    rospy.init_node('print_tracker_pose')
+    odom_sub = rospy.Subscriber('/vive/LHR_BD4ED973_odom', Odometry, get_current_position)
+    rospy.spin()
---
+++ scripts/get_tracker_pose.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+'''
+0.2 = 20cm
+1 = 1m
+the y is wrong 
+'''
+import rospy
+from geometry_msgs.msg import PoseWithCovarianceStamped
+
+def get_current_position(msg):
+    # follows the conventional x, y, poses
+    pose_position = msg.pose.pose.position
+    position_x = pose_position.x
+    position_y = pose_position.y
+    position_z = pose_position.z
+
+    pose_orientation = msg.pose.pose.orientation
+    orientation_x = pose_orientation.x
+    orientation_y = pose_orientation.y
+    orientation_z = pose_orientation.z
+
+    import time
+    seconds = int(round(time.time()*10))
+    # time.sleep(0.5)
+    # print(seconds)
+    if seconds % 10 == 0:
+
+        print("x: {:.3f} y: {:.3f} w: {:.3f}".format(position_x, position_y, position_z ))
+        print("x: {:.3f} y: {:.3f} w: {:.3f}".format(orientation_x, orientation_y, orientation_z ))
+        print("=======================================================================")
+
+    else:
+        pass
+        
+
+
+if __name__=="__main__":
+    
+    rospy.init_node('print_tracker_pose')
+    odom_sub = rospy.Subscriber('/vive/LHR_BD4ED973_pose', PoseWithCovarianceStamped, get_current_position)
+    rospy.spin()
